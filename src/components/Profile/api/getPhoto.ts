178d9924c58ff51191411ea9dcaea8a1d3f489c9
DaniilVolobuiev <danalex999@ukr.net>
2023-05-01 13:38:56 +0200

    fix

--- src/components/Profile/api/getPhoto.ts
+++ src/components/Profile/api/getPhoto.ts
@@ -1,7 +1,11 @@
 import axios from './axios';
 
 export const getDoctorAvatar = async (doctorId: number) => {
-  const url = `/doctor/${doctorId}/avatar`;
-  const response = await axios.get(url, {});
-  return response.data;
+  try {
+    const url = `/doctor/${doctorId}/avatar`;
+    const response = await axios.get(url, {});
+    return response.data;
+  } catch (error) {
+    `The following error occured while trying to get photo: ${error}`;
+  }
 };
