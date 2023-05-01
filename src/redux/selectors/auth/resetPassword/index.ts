cda1de49e1084e753b716792797c09ba71a1f29a
Oleg Razno <6868062@stud.nau.edu.ua>
2023-05-01 17:40:22 +0300

    fix: removed unused selectors

--- src/redux/selectors/auth/resetPassword/index.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { RootState } from '@redux/store';
-
-export const selectResetPassword = createSelector(
-  (state: RootState) => state,
-  (state) => state.resetPassword,
-);
-
-export const mock = '';
