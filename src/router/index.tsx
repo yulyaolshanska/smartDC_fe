24e414d2916fec605c048f86ee855e03e5c71f5f
Oleg Razno <6868062@stud.nau.edu.ua>
2023-05-04 21:37:59 +0300

    fix: fixed the check for errors in the login form and Input city in the profile, deleted unnecessary character

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -42,6 +42,8 @@ const AppRouter = () => {
         <Route path={PATH.VERIFICATION} element={<Activation />} />
         <Route path={PATH.LOGIN} element={<Login />} />
         <Route path={PATH.RESET_PASS} element={<ResetPassword />} />
+        <Route path={PATH.FORGOT_PASS} element={<ForgotPassword />} />
+        <Route path={PATH.CONFIRM} element={<Confirmation />} />
 
         {/* Private Routes */}
         <Route
@@ -57,22 +59,6 @@ const AppRouter = () => {
           element={
             <ProtectedRoute allowedRoles={['Remote', 'Local']}>
               <CreatePatientCard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={PATH.FORGOT_PASS}
-          element={
-            <ProtectedRoute allowedRoles={['Remote', 'Local']}>
-              <ForgotPassword />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path={PATH.CONFIRM}
-          element={
-            <ProtectedRoute allowedRoles={['Remote', 'Local']}>
-              <Confirmation />
             </ProtectedRoute>
           }
         />
