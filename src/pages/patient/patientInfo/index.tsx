cd2d3372e5630b3f6dd1d110d78c605e6396f582
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-05-30 21:57:57 +0300

    remove zoom

--- src/pages/patient/patientInfo/index.tsx
+++ src/pages/patient/patientInfo/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import PatientCardInfo from '@components/Patient/PatientInfo';
 import WeeklyCalendar from '@components/WeeklyCalendar';
-import ZoomComponent from '@components/Zoom';
 import Notes from '@components/Notes';
 import GoBackLink from '@components/GoBackLink';
 
 const PatientInfo = () => {
   return (
     <>
-      <ZoomComponent />
       <GoBackLink />
       <PatientCardInfo />
       <WeeklyCalendar />
