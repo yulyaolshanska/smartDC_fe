b7f5c67439e0f383888ecbb2d1521ca7d37ec6ec
Oleg Razno <6868062@stud.nau.edu.ua>
2023-06-04 18:15:39 +0300

    fix: fixed appointments route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -121,7 +121,7 @@ const AppRouter = () => {
           }
         />
         <Route
-          path={PATH.APPOINTMENT}
+          path={PATH.APPOINTMENTS}
           element={
             <ProtectedRoute allowedRoles={['Remote', 'Local']}>
               <AppointmentsDoctorScheduler />
