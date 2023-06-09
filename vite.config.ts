dae3fe0a1258b8f2e0376f02338444979a90c81f
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-06-09 17:51:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,4 @@ export default defineConfig({
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
-  optimizeDeps: {
-    exclude: ['js-big-decimal']
-  }
 });
